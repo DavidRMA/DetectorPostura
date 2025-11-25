658dec3758e41239695e1c2212f1439c78253834
DavidRMA <165867613+DavidRMA@users.noreply.github.com>
2025-11-24 22:44:09 -0500

    configuraciones adicionales

--- DetectorPostura/DetectorPostura/settings.py
+++ DetectorPostura/DetectorPostura/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import shutil
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,11 +75,20 @@ WSGI_APPLICATION = 'DetectorPostura.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
+if shutil.which('psql') is None:
+    os.environ['PATH'] += os.pathsep + r"C:\Program Files\PostgreSQL\18\bin"
+
+# Ruta absoluta al archivo de credenciales
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
